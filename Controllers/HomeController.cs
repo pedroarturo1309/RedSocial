bb7993ad0772d601bc24c05727c8a6ff6beab455
PedroArturo <deleonarturo09@gmail.com>
2017-11-11 15:54:13 -0400

    asd
    sad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace RedSocial.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Loquesea = "ASKDALSKDASd";
+            ViewBag.Loquesea = "ASKDALSKDAS";
             return View();
         }
 
